6737508d6dd8c289181572364ca9f0a7a1262719
unknown <vk9944836332@gmail.com>
2024-01-09 17:29:54 +0530

    ruthra

---
+++ script.js
@@ -0,0 +1,66 @@
+
+/* ----- TYPING EFFECT ----- */
+ let typingEffect = new Typed(".typedText",{
+    strings : ["FrontEndDeveloper"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+sr.reveal('.skills-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+srLeft.reveal('.html',{delay: 100})
+
+srLeft.reveal('.bootstrap',{delay: 100})
+
+srLeft.reveal('.react',{delay: 100})
+
+
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+srRight.reveal('.css',{delay: 100})
+srRight.reveal('.javascript',{delay: 100})
+srRight.reveal('.github',{delay: 100})
